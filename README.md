f4415b9e440791b1ac442b53678788b962f44320
Matthew Geary <mgear2@pdx.edu>
2019-07-13 19:17:00 -0700

    Set up project (thanks cargo), first README version, main.rs takes user input and is ready to be built off of

---
+++ README.md
@@ -0,0 +1,17 @@
+# rust-irc
+Copyright (c) 2019 Matthew geary
+
+A simple IRC (Internet Relay Chat) Client written in Rust. 
+For connecting to freenode and possibly other irc networks. 
+
+## Usage
+
+`cargo run <username> <channel>`
+
+Run `cargo test` to do some simple testing. 
+
+## License
+
+This program is licensed under the "MIT License".  Please
+see the file `LICENSE` in the source distribution of this
+software for license terms.
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+// Copyright © 2019 Matthew Geary
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+use std::process::exit;
+
+///! A simple IRC client written in Rust. 
+
+/// Report proper usage and exit.
+fn usage() -> ! {
+    eprintln!("rust-irc: usage: rust-irc [username] [channel]");
+    exit(1);
+}
+
+///```
+///assert_eq!(Some("test"), Some("test"))
+///```
+fn send(msg: String) {
+    println!("Sent: {}", msg);
+}
+
+/// Do the computation.
+fn main() {
+    // Process the arguments.
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 3 {
+        usage();
+    }
+    let username = &args[1];
+    let channel = &args[2];
+
+    println!("username: {}, channel: {}", username, channel);
+
+    // Read the input.
+    use std::io;
+    loop {
+        let mut msg = String::new();
+        match io::stdin().read_line(&mut msg) {
+            Ok(n) => {
+                msg = msg.trim().to_string();
+                println!("{} bytes read", n);
+                if msg == "exit" {
+                    println!("exiting...");
+                    break;
+                }
+                else {
+                    send(msg);
+                }
+            }
+            Err(error) => eprintln!("error: {}", error),
+        }
+    }
+}
+
+mod tests {
+    //use super::*;
+    
+    // extremely important test
+    #[test]
+    fn extra_testy_test() {
+        assert_eq!((2+2), 4)
+    }
+}
