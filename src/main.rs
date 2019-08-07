3400ed1f3af02adfaf5625fd72267b68190b61ca
Matthew <mgear2@pdx.edu>
2019-08-06 17:00:57 -0700

    Fixed issue resulting from using two different TcpStreams (and thus two sockets); now use TcpStream method try_clone() to create an second independently owned handle to a single socket

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,19 @@ fn usage() -> ! {
     exit(1);
 }
 
-fn connect(nick: String) -> (std::io::Result<TcpStream>, std::io::Result<TcpStream>) {
+fn connect(nick: String) -> std::io::Result<TcpStream> {
     // https://doc.rust-lang.org/std/net/struct.TcpStream.html
-    let recv_stream = TcpStream::connect("irc.freenode.org:6667").unwrap();
-    let send_stream = TcpStream::connect("irc.freenode.org:6667").unwrap();
-    //stream.set_nonblocking(true).expect("set_nonblocking call failed");
+    let send_stream = TcpStream::connect("irc.freenode.net:6667")?;
 
     // https://tools.ietf.org/html/rfc1459#section-4.1.1
     // https://github.com/hoodie/concatenation_benchmarks-rs
     let nick_string = format!("{}\r\n", &nick);
     let user_string = format!("{} * * {}\n\r", &nick, &nick);
 
-    let _ = send_cmd(&recv_stream, "USER", user_string);
-    let _ = send_cmd(&recv_stream, "NICK", nick_string);
+    let _ = send_cmd(&send_stream, "USER", user_string);
+    let _ = send_cmd(&send_stream, "NICK", nick_string);
 
-    (Ok(recv_stream), Ok(send_stream))
+    Ok(send_stream)
 }
 
 fn _send_msg(mut stream: &TcpStream, msg: String) -> Result<usize, std::io::Error> {
@@ -45,7 +43,10 @@ fn send_cmd(mut stream: &TcpStream, cmd: &str, msg: String) -> Result<usize, std
     let mut cmd = cmd.to_string();
     cmd.push_str(" ");
     cmd.push_str(&msg);
-    stream.write(cmd.as_bytes())
+    println!("sending: {}", cmd.trim());
+    let res = stream.write(cmd.as_bytes());
+    println!("{:?}", res);
+    return res
 }
 
 fn receive(mut stream: &TcpStream) {
@@ -56,14 +57,17 @@ fn receive(mut stream: &TcpStream) {
         for _ in 0..512 {
             let _ = stream.read_exact(&mut temp);
             match temp[0] {
+                0x1 => continue, // start of heading
                 0xD => continue, // carriage return
                 0xA => break,    // line feed
                 _ => buffer.push(temp[0]),
             }
         }
         let res_string = str::from_utf8(&buffer[..]).unwrap();
-        println!("{}: {}", i, res_string);
-        i += 1;
+        if !res_string.is_empty() {
+            println!("{}: {}", i, res_string);
+            i += 1;
+        }
     }
 }
 
@@ -76,9 +80,8 @@ fn main() -> std::io::Result<()> {
     }
     let nick = args[1].clone();
     let _channel = &args[2];
-    let streams = connect(nick);
-    let recv_stream = streams.0.unwrap();
-    let send_stream = streams.1.unwrap();
+    let send_stream = connect(nick)?;
+    let recv_stream = send_stream.try_clone()?;
 
     // https://doc.rust-lang.org/nightly/std/thread/
     thread::spawn( move || {
@@ -91,14 +94,19 @@ fn main() -> std::io::Result<()> {
         let mut msg = String::new();
         match io::stdin().read_line(&mut msg) {
             Ok(_) => {
+                // https://users.rust-lang.org/t/how-to-split-a-string-by-and-then-print-first-or-last-component/23042
                 let msg: Vec<&str> = msg.trim().split(" ").collect();
-                let cmd: &str = msg[0];
+                let cmd: &str = msg[0].trim();
                 match cmd {
                     "/quit" => {
                         let _ = send_cmd(&send_stream, "QUIT", "\r\n".to_string());
                         println!("Quitting...");
                         return Ok(());
                     },
+                    "/join" => {
+                        let join_msg = format!("{}\r\n", msg[1].trim());
+                        let _ = send_cmd(&send_stream, "JOIN", join_msg);
+                    }
                     _ => {
                         println!("Unrecognized command: {}", msg[0]);
                     }
